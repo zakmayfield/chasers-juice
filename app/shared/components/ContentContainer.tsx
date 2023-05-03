ea71c01c9d7fc588aa61a5d31ef5015e91775b36
Dani Mayfield <dani@neon.cc>
2023-05-03 16:25:09 -0400

    chore: update ContentContainer + add notes

--- app/shared/components/ContentContainer.tsx
+++ app/shared/components/ContentContainer.tsx
@@ -9,7 +9,11 @@ export const ContentContainer = ({
   ...props
 }: ContentContainerProps) => {
   return (
-    <Container maxW="container.lg" p="0" m="0" mx="auto" {...props}>
+    /** Note: make sure to leave a px on the ContentContainer so all components
+     * with content have the same padding. Use p="0" for components that don't
+     * need padding.
+     */
+    <Container maxW="container.lg" px="8" m="0" mx="auto" {...props}>
       {children}
     </Container>
   );
